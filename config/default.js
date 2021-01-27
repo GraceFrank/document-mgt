99b6551961da6310c65a2c0f0a8a31bf269ed67d
Grace Frank <grace.frank@centricityinsights.com>
2021-01-27 13:59:23 +0100

    updated start script

--- config/default.js
+++ config/default.js
@@ -1,17 +1,18 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const testConfig = {
-  db: 'mongodb://localhost/gdocs_test',
-  privateKey: '1234',
-  redisHost: 'localhost',
-  redisPort: '6379'
+  db: "mongodb://localhost/gdocs_test",
+  privateKey: "1234",
+  redisHost: "localhost",
+  redisPort: "6379",
 };
 
 const defaultConfig = {
   db: process.env.DATABASE_URL,
   privateKey: process.env.API_PRIVATE_KEY,
   redisHost: process.env.REDIS_HOST,
-  redisPort: process.env.REDIS_PORT
+  redisPort: process.env.REDIS_PORT,
+  redisPassword: process.env.REDIS_PASSWORD || "",
 };
 
-module.exports = process.env.NODE_ENV === 'test' ? testConfig : defaultConfig;
+module.exports = process.env.NODE_ENV === "test" ? testConfig : defaultConfig;
--- server/startup/cache.js
+++ server/startup/cache.js
@@ -1,18 +1,19 @@
 //Connection to Redis client
-const redis = require('redis');
-const logger = require('./logger');
-const util = require('util');
-const config = require('../../config/default');
+const redis = require("redis");
+const logger = require("./logger");
+const util = require("util");
+const config = require("../../config/default");
 
 const redisClient = redis.createClient({
   host: config.redisHost,
-  port: config.redisPort
+  port: config.redisPort,
+  password: config.redisPassword,
 });
 redisClient.get = util.promisify(redisClient.get);
 
 function connectToRedis() {
-  redisClient.on('connect', () => logger.info('connected to redis server'));
-  redisClient.on('error', err => logger.error(err));
+  redisClient.on("connect", () => logger.info("connected to redis server"));
+  redisClient.on("error", (err) => logger.error(err));
 }
 
 module.exports = { redisClient, connectToRedis };
