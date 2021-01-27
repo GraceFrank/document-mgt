84322f34d1bde8a9aa6fc059e644f18f432f346f
Grace Frank <grace.frank@centricityinsights.com>
2021-01-27 20:51:01 +0100

    added random

--- server/startup/routes.js
+++ server/startup/routes.js
@@ -1,18 +1,19 @@
-const express = require('express');
-const roles = require('../routes/roles');
-const users = require('../routes/users');
-const login = require('../routes/login');
-const documents = require('../routes/document');
-const swagger = require('../documentation/swagger');
+const express = require("express");
+const roles = require("../routes/roles");
+const users = require("../routes/users");
+const login = require("../routes/login");
+const documents = require("../routes/document");
+const swagger = require("../documentation/swagger");
 
 function routes(app) {
   app.use(express.json());
 
-  app.use('/api/roles', roles);
-  app.use('/api/users', users);
-  app.use('/api/login', login);
-  app.use('/api/documents', documents);
-  app.use('/api-docs', swagger);
+  app.use("/api/roles", roles);
+  app.use("/api/users", users);
+  app.use("/api/login", login);
+  app.use("/api/documents", documents);
+  app.use("/api-docs", swagger);
 }
+const x = "";
 
 module.exports = routes;
